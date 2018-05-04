5a997dced3e76c8e3f6c0b22a9b7a1667da10ed9
kyle <yxu49@ucsc.edu>
2018-05-05 01:48:27 +0800

    aaa

--- Lab2/pintos/src/threads/condvar.c
+++ Lab2/pintos/src/threads/condvar.c
@@ -76,8 +76,8 @@ cond_sema_cmp_priority (const struct list_elem *a, const struct list_elem *b, vo
 {
     bool result;
 
-  struct semaphore_elem *sema_a = list_entry (a, struct semaphore_elem, elem);
-  struct semaphore_elem *sema_b = list_entry (b, struct semaphore_elem, elem);
+  struct semaphore *sema_a = list_entry (a, struct semaphore, elem);
+  struct semaphore *sema_b = list_entry (b, struct semaphore, elem);
   int sema_a_p=list_entry(list_front(&sema_a->waiters),struct thread,elem)->priority;
   int sema_b_p=list_entry(list_front(&sema_b->waiters),struct thread,elem)->priority;
   if (sema_a_p>sema_b_p){
