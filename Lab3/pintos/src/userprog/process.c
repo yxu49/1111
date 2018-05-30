dd1a0fcf12be3b14196c7768ca2904eb22896c01
yxu49 <yxu49@ucsc.edu>
2018-05-30 14:55:33 -0700

    aa

--- Lab3/pintos/src/userprog/process.c
+++ Lab3/pintos/src/userprog/process.c
@@ -59,7 +59,7 @@
 
 static thread_func start_process NO_RETURN;
 static bool load(const char *cmdline, void (**eip)(void), void **esp);
-static struct semaphore locking;
+// static struct semaphore locking;
 /*
  * Push the command and arguments found in CMDLINE onto the stack, world 
  * aligned with the stack pointer ESP. Should only be called after the ELF 
